6b5588daafeee55422ba9618c22042108d597c90
Wolfram Reinke <WolframReinke@web.de>
2016-06-16 23:23:21 +0200

    Fixed color codes, added BG colors to color preview
    The color codes for background colors were all wrong, I must have copied
    them from somewhere without thinking.  The new codes actually work and
    the naming is the same as for the foreground colors.
    
    The color preview function 'showcase_colors' didn't include background
    colors, now it does.

--- scripts/lib/colors.sh
+++ scripts/lib/colors.sh
@@ -37,18 +37,25 @@ export FG_COLOR_RESET='\033[0m'
 #-------------------------------------------------------------------------------
 # BACKGROUND COLORS
 #-------------------------------------------------------------------------------
-export BG_COLOR_BLACK='\033[40;XXm'
-export BG_COLOR_RED='\033[41;XXm'
-export BG_COLOR_GREEN='\033[42;XXm'
-export BG_COLOR_BROWN='\033[43;XXm'
-export BG_COLOR_YELLOW=$BG_COLOR_BROWN
-export BG_COLOR_BLUE='\033[44;XXm'
-export BG_COLOR_PURPLE='\033[45;XXm'
-export BG_COLOR_VIOLET=$BG_COLOR_PURPLE
-export BG_COLOR_CYAN='\033[46;XXm'
-export BG_COLOR_TURQUOISE=$BG_COLOR_CYAN
-export BG_COLOR_GRAY='\033[47;XXm'
-export BG_COLOR_RESET='\033[XXm'
+export BG_COLOR_BLACK='\033[40m'
+export BG_COLOR_RED_DARK='\033[41m'
+export BG_COLOR_GREEN_DARK='\033[42m'
+export BG_COLOR_YELLOW_DARK='\033[43m'
+export BG_COLOR_BROWN=$BG_COLOR_YELLOW_DARK
+export BG_COLOR_BLUE_DARK='\033[44m'
+export BG_COLOR_MAGENTA_DARK='\033[45m'
+export BG_COLOR_CYAN_DARK='\033[46m'
+export BG_COLOR_GRAY='\033[47m'
+export BG_COLOR_GRAY_DARK='\033[100m'
+export BG_COLOR_RED='\033[101m'
+export BG_COLOR_GREEN='\033[102m'
+export BG_COLOR_YELLOW='\033[103m'
+export BG_COLOR_BLUE='\033[104m'
+export BG_COLOR_MAGENTA='\033[105m'
+export BG_COLOR_CYAN='\033[106m'
+export BG_COLOR_WHITE='\033[107m'
+export BG_COLOR_GREEN='\033[42m'
+export BG_COLOR_RESET='\033[49m'
 
 
 function unsource_colors {
@@ -74,18 +81,26 @@ function unsource_colors {
     unset FG_COLOR_GRAY
     unset FG_COLOR_WHITE
     unset FG_COLOR_RESET
+
     unset BG_COLOR_BLACK
+    unset BG_COLOR_RED_DARK
+    unset BG_COLOR_GREEN_DARK
+    unset BG_COLOR_YELLOW_DARK
+    unset BG_COLOR_BROWN
+    unset BG_COLOR_BLUE_DARK
+    unset BG_COLOR_MAGENTA_DARK
+    unset BG_COLOR_CYAN_DARK
+    unset BG_COLOR_GRAY
+    unset BG_COLOR_GRAY_DARK
     unset BG_COLOR_RED
     unset BG_COLOR_GREEN
-    unset BG_COLOR_BROWN
     unset BG_COLOR_YELLOW
     unset BG_COLOR_BLUE
-    unset BG_COLOR_PURPLE
-    unset BG_COLOR_VIOLET
+    unset BG_COLOR_MAGENTA
     unset BG_COLOR_CYAN
-    unset BG_COLOR_TURQUOISE
+    unset BG_COLOR_WHITE
+    unset BG_COLOR_GREEN
     unset BG_COLOR_RESET
-    unset BG_COLOR_GRAY
 }
 
 function showcase_colors {
@@ -104,6 +119,7 @@ function showcase_colors {
     echo -e "\t ${FG_COLOR_PURPLE_DARK}fg_color_purple_dark${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_VIOLET_DARK}fg_color_violet_dark${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_PURPLE}fg_color_purple${FG_COLOR_RESET}"
+    echo -e "\t ${FG_lib}fg_color_violet${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_VIOLET}fg_color_violet${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_CYAN_DARK}fg_color_cyan_dark${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_TURQUOISE_DARK}fg_color_turquoise_dark${FG_COLOR_REST}"
@@ -111,4 +127,24 @@ function showcase_colors {
     echo -e "\t ${FG_COLOR_TURQUOISE}fg_color_turquoise${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_GRAY}fg_color_gray${FG_COLOR_RESET}"
     echo -e "\t ${FG_COLOR_WHITE}fg_color_white${FG_COLOR_RESET}"
+
+    echo "Background Colors"
+    echo -e "\t ${BG_COLOR_BLACK}bg_color_black${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_RED_DARK}bg_color_red_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_GREEN_DARK}bg_color_green_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_YELLOW_DARK}bg_color_yellow_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_BROWN}bg_color_brown${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_BLUE_DARK}bg_color_blue_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_MAGENTA_DARK}bg_color_magenta_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_CYAN_DARK}bg_color_cyan_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_GRAY}bg_color_gray${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_GRAY_DARK}bg_color_gray_dark${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_RED}bg_color_red${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_GREEN}bg_color_green${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_YELLOW}bg_color_yellow${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_BLUE}bg_color_blue${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_MAGENTA}bg_color_magenta${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_CYAN}bg_color_cyan${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_WHITE}bg_color_white${BG_COLOR_RESET}"
+    echo -e "\t ${BG_COLOR_GREEN}bg_color_green${BG_COLOR_RESET}"
 }
