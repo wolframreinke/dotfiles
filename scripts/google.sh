fa103bad70f1e94cd1418dd5e9ff5f0d113fb5af
Wolfram Reinke <WolframReinke@web.de>
2016-11-04 18:04:49 +0100

    google.sh now works without X
    Also, it now uses all arguments in a single query, so google.sh a b c
    will google "a b c" instead of googling a b and c individually.

--- scripts/google.sh
+++ scripts/google.sh
@@ -2,8 +2,30 @@
 #
 # opens firefox to display google results.
 
-while [ $# -ne 0 ]
-do
-    firefox "https://www.google.com/search?q=$1&gws_rd=ssl" & > /dev/null 2>&1
-    shift
-done
+urlencode() {
+    local string="${@}"
+    local strlen=${#string}
+    local encoded=""
+    local pos c o
+
+
+    for (( pos=0 ; pos<strlen ; pos++ )); do
+        c=${string:$pos:1}
+        case "$c" in
+            [-_.~a-zA-Z0-9] ) o="${c}";;
+            * )               printf -v o '%%%02x' "'$c"
+        esac
+        encoded+="${o}"
+    done
+
+    echo ${encoded}
+}
+
+query="$(urlencode ${@})"
+url="https://www.google.com/search?q=${query}&gws_rd=ssl"
+
+if [ -z "${DISPLAY}" ]; then
+    w3m ${url}
+else
+    firefox ${url} &  > /dev/null 2>&1
+fi
